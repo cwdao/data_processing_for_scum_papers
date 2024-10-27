607b6623dd1d0c22e2234a5e62d583f00bb4670f
Cheng WANG <cwang199@connect.hkust-gz.edu.cn>
2024-10-27 17:39:13 +0800

    update point calculation

--- scm_j_location_acc_processor.m
+++ scm_j_location_acc_processor.m
@@ -1,3 +1,39 @@
+% 加载数据
+clc;
+clear;
+
+load("data_2024-10-26_16-28_p2point.mat");
+%%
+% 取点数据
+ax = data(:,1);
+ay = data(:,2);
+point_label = data(:,5);
+% 提取前两个校正点，首先给了标签9和8，分别是左上和右下的点
+calib_left= [];
+i = 1;
+j = 1;
+k = 1;
+% 循环条件希望在看到第一个1标签之后就停下来，以免重复，后面的8，9不是校正点
+while (point_label(i,1) ~=1)
+    if (point_label(i,1)  == 9)
+        calib_left(j,1) = ax(i,1);
+        calib_left(j,2) = ay(i,1);
+        j = j +1;
+    end
+    if (point_label(i,1)  == 8)
+        calib_right(k,1) = ax(i,1);
+        calib_right(k,2) = ay(i,1);
+        k = k+1;
+    end
+    i = i+1;
+end
+
+point_calib_l = mean(calib_left);
+point_calib_r = mean(calib_right);
+
+
+%% 更换代码
+
 % 理想点
 ideal_points = [50, 350; 150, 250; 250, 150; 350, 50];
 
@@ -46,4 +82,28 @@ function plot_error_ellipse(C, mu)
     t = linspace(0, 2*pi, 100);
     a = (V * sqrt(D)) * [cos(t(:))'; sin(t(:))'];
     plot(a(1, :) + mu(1), a(2, :) + mu(2), 'r--', 'LineWidth', 1);
+end
+
+function calculate_position()
+# 计算实际位置需要的参数
+lighthouse_height = 550  # 单位mm
+lighthouse_freq = 120  # 120Hz for sync light
+lighthouse_period = 1 / lighthouse_freq  # 120Hz~=0.00833s
+lighthouse_angular_velocity = 2 * math.pi * lighthouse_freq
+resolution = 10000000  # @10M,1s= 10,000,000 ticks
+
+
+# 当前版本的角度计算方法是一种更易理解的形式，即时间/周期*pi。
+# 获得角度后，根据简单的三角函数方法就能计算得到相应的X，Y相对坐标。
+def get_position(time_x, time_y, height_lh, resolution):
+
+    time_motor_ax = time_x / resolution  # seconds
+    time_motor_ay = time_y / resolution
+    theta_ax = time_motor_ax / lighthouse_period * math.pi  # now in radians
+    theta_ay = time_motor_ay / lighthouse_period * math.pi
+    max_side_x = height_lh / math.sin(theta_ax)
+    max_side_y = height_lh / math.sin(theta_ay)
+    x_p = max_side_x * math.cos(theta_ax)
+    y_p = max_side_y * math.cos(theta_ay)
+    return x_p, y_p
 end
